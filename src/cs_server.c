29e4ce964c268b1932aa8710e6ce9306ff1790c4
troy <troyyuan.smit@gmail.com>
2013-11-16 23:46:44 +0800

    fill in the main code

--- src/cs_server.c
+++ src/cs_server.c
@@ -7,17 +7,101 @@
 
 int main(int argc, char *argv[])
 {
-    D("hello world.");
+	D("hello world.");
 
-	int socket(int domain, int type, int protocol);
-	int setsockopt(int sockfd, int level, int optname, const void *optval, socklen_t optlen);
-	int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-	int listen(int sockfd, int backlog);
-	int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
-	ssize_t read(int fd, void *buf, size_t count);
-	ssize_t write(int fd, const void *buf, size_t count);
-	ssize_t read(int fd, void *buf, size_t count);
-	int close(int fd);
+	int sockfd = -1;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		E("%s", strerror(errno));
+		return -1;
+	}
+
+	int optval = 1;
+	int ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+			&optval, sizeof(optval));
+	if (ret == -1) {
+		E("%s", strerror(errno));
+		return -1;
+	}
+	//ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT,
+			//&optval, sizeof(optval));
+	
+	struct sockaddr_in addr;
+	socklen_t addrlen = sizeof(addr);
+
+	memset(&addr, '\0', addrlen);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8888);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	ret = bind(sockfd, (struct sockaddr *)&addr, addrlen);
+	if (ret == -1) {
+		E("%s", strerror(errno));
+		return -1;
+	}
+
+	int backlog = 20;
+	ret = listen(sockfd, backlog);
+	if (ret == -1) {
+		E("%s", strerror(errno));
+		return -1;
+	}
+
+	struct sockaddr_in peer_addr;
+	socklen_t peer_addrlen = sizeof(peer_addr);
+
+	memset(&peer_addr, '\0', peer_addrlen);
+
+	size_t buflen = 512;
+	char *buf = (char *)cs_malloc(sizeof(char) * buflen);
+	if (buf == NULL) {
+		E("cs_malloc() failed.");
+		return -1;
+	}
+
+	int peer_sockfd = -1;
+	ssize_t s = 0;
+	while (1) {
+		peer_sockfd = accept(sockfd,
+				(struct sockaddr *)&peer_addr, &peer_addrlen);
+		if (peer_sockfd == -1) {
+			E("%s", strerror(errno));
+			cs_free(&buf);
+			return -1;
+		}
+
+#if 0
+		s = read(sockfd, buf, buflen);
+		if (s == -1) {
+			E("%s", strerror(errno));
+			cs_free(&buf);
+			return -1;
+		}
+
+		DS(buf);
+#endif
+
+#if 1
+		strncpy(buf, "hi\n", 3);
+
+		s = write(peer_sockfd, buf, strlen(buf));
+		if (s == -1) {
+			E("%s", strerror(errno));
+			cs_free(&buf);
+			return -1;
+		}
+#endif
+
+		memset(buf, '\0', buflen);
+	}
+
+	cs_free(&buf);
+
+	ret = close(sockfd);
+	if (ret == -1) {
+		E("%s", strerror(errno));
+		return -1;
+	}
 
 #if 0
 	rscallback();
@@ -29,8 +113,9 @@ int main(int argc, char *argv[])
 	int  FD_ISSET(int fd, fd_set *set);
 	void FD_SET(int fd, fd_set *set);
 	void FD_ZERO(fd_set *set);
-	int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
+	int select(int nfds, fd_set *readfds, fd_set *writefds,
+			fd_set *exceptfds, struct timeval *timeout);
 #endif
 
-    return 0;
+	return 0;
 }
